fe79a21c6096217af1187cf0d2ec81c4fa7bc6e5
mosesmbuthia <mbuthiamoses435@gmail.com>
2025-03-09 11:58:49 +0300

    build the about section

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ function Home() {
   return (
     <div>
       <Hero/>
+      <About/>
     </div>
   )
 }
@@ -13,8 +14,19 @@ export default Home
 function Hero(){
     return(
       <div className="hero-main">
-        <h1 className="main-line">welcome to moses safaris</h1>
-        <p className="sub-line">Best safaris and adventures</p>
+        <p className="main-line">welcome to moses safaris</p>
+        <h1 className="sub-line">Best way to find your dream safari adventures</h1>
+        
       </div>  
+    )
+}
+
+function About() {
+    return(
+        <div className="hero-about">
+            <h2 className="about-main-tag">About Us</h2>
+            <p className="about-content">Africa has a safari for every traveller – what will your travel story be? African safari packages and tours range from bucket-list adventures to ultra-luxurious vacations and everything in between. Use the links below to find family-friendly vacations, romantic honeymoon packages, thrilling African safaris and affordable holidays. Remember, all our itineraries are totally flexible and can be changed to suit you. No time to browse? Get in touch with an Moses Safaris Expert to plan a tailor-made tour around your interests, wishes and budget.</p>
+
+        </div>
     )
 }
